7a01bd3bb6d09714e60a73d2ad9755104499799a
jongsu253 <jongsu253@gmail.com>
2019-10-08 04:02:43 +0900

    Give data layer

---
+++ app/src/main/java/com/buzzvil/rs/hellokotlin/data/model/Brewery.kt
@@ -0,0 +1,11 @@
+package com.buzzvil.rs.hellokotlin.data.model
+
+data class Brewery(
+    val id: Int,
+    val name: String,
+    val breweryType: String,
+    val street: String,
+    val city: String,
+    val state: String,
+    val phone: String
+)
