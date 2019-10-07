6a72c48d2e14b91160fcdbceb3094b842637d9ae
jongsu253 <jongsu253@gmail.com>
2019-10-07 19:21:31 +0900

    Add external dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,4 +21,19 @@ android {
 
 dependencies {
     api(fileTree("libs") { include("*.jar") })
+
+    api(Config.Libs.Androidx.recyclerView)
+
+    api(Config.Libs.Others.rxJava2)
+    api(Config.Libs.Others.rxAndroid)
+
+    api(Config.Libs.Data.gson)
+    api(Config.Libs.Data.gsonConverter)
+    api(Config.Libs.Data.rxJava2Adapter)
+    api(Config.Libs.Data.okhttp)
+    api(Config.Libs.Data.okhttpLoggingInterceptor)
+    api(Config.Libs.Data.retrofit2)
+
+    testImplementation(Config.Libs.Test.junit)
+    testImplementation(Config.Libs.Test.mockk)
 }
--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -13,6 +13,23 @@ object Config {
     }
 
     object Libs {
+        object Data {
+            const val gson = "com.google.code.gson:gson:2.8.2"
+            const val gsonConverter = "com.squareup.retrofit2:converter-gson:2.3.0"
+            const val rxJava2Adapter = "com.squareup.retrofit2:adapter-rxjava2:2.3.0"
+            const val okhttp = "com.squareup.okhttp3:okhttp:3.9.0"
+            const val okhttpLoggingInterceptor = "com.squareup.okhttp3:logging-interceptor:3.9.0"
+            const val retrofit2 = "com.squareup.retrofit2:retrofit:2.3.0"
+        }
+
+        object Others {
+            private const val rxJava2Version = "2.2.13"
+            private const val rxAndroidVersion = "2.1.1"
+
+            const val rxJava2 = "io.reactivex.rxjava2:rxjava:$rxJava2Version"
+            const val rxAndroid = "io.reactivex.rxjava2:rxandroid:$rxAndroidVersion"
+        }
+
         object Kotlin {
             private const val kotlinCoroutineVersion = "1.2.1"
 
@@ -24,14 +41,17 @@ object Config {
         object Androidx {
             private const val appcompatVersion = "1.0.2"
             private const val coreKtxVersion = "1.0.2"
+            private const val recyclerViewVersion = "1.0.0"
 
             const val appcompat = "androidx.appcompat:appcompat:$appcompatVersion"
             const val coreKtx = "androidx.core:core-ktx:$coreKtxVersion"
+            const val recyclerView = "androidx.recyclerview:recyclerview:$recyclerViewVersion"
         }
 
         object Test {
             const val junit = "junit:junit:4.12"
             const val mockitoCore = "org.mockito:mockito-core:2.19.1"
+            const val mockk = "io.mockk:mockk:1.9.3.kotlin12"
         }
     }
 }
